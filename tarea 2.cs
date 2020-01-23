db81c77e865910b4c9ac60ab5450643e02cc22f6
marianaCano <60165489+marianaCano@users.noreply.github.com>
2020-01-22 22:59:33 -0500

    Add files via upload

---
+++ tarea 2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace tarea1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ingrse t y a");
+            double t = double.Parse(Console.ReadLine());
+            double aGrados = double.Parse(Console.ReadLine());
+
+            double a = aGrados * (Math.PI / 180);
+           
+            
+            double c = 180 -(aGrados + 90);
+            Console.WriteLine("c:");
+            Console.WriteLine(c);
+
+            double z = (t * Math.Cos(a));
+            Console.WriteLine("z:");
+            Console.WriteLine(z);
+
+            double y = (t * Math.Sin(a));
+            Console.WriteLine("y:");
+            Console.WriteLine(y);
+        }
+    }
+}
