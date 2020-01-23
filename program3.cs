1a14892ec958430f5a650e36df61bcf2ed683217
marianaCano <60165489+marianaCano@users.noreply.github.com>
2020-01-22 22:58:02 -0500

    Add files via upload

---
+++ program3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace tarea1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ingrse z y c ");
+            double z = double.Parse(Console.ReadLine());
+            double cGrados = double.Parse(Console.ReadLine());
+
+            double c = cGrados * (Math.PI / 180.0);
+
+
+            double a = 180.0 - (cGrados + 90);
+            Console.WriteLine("a:");
+            Console.WriteLine(a);
+
+            double y = (z / Math.Tan(c));
+            Console.WriteLine("y:");
+            Console.WriteLine(y);
+
+
+            double t = Math.Sqrt(Math.Pow(z, 2) + Math.Pow(y, 2));
+            Console.WriteLine("t:");
+            Console.WriteLine(t);
+
+        }
+    }
+}
