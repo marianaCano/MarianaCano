4dbc7eccc946c888d1e36f7fce47e4cba6c60759
marianaCano <60165489+marianaCano@users.noreply.github.com>
2020-01-29 14:21:50 -0500

    Add files via upload

---
+++ Program31.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ingreso de datos
+            Console.WriteLine(" ingrse su salario");
+            double smmlv = double.Parse(Console.ReadLine());
+            double smmlv2 = 877803 * 2;
+            double smmlv3 = 877803 * 4;
+
+           
+
+            if (smmlv < smmlv2 )
+            { 
+                Console.WriteLine("tarifa A");
+            }
+            else if ( smmlv2 <= smmlv && smmlv <= smmlv3 )
+            {
+                Console.WriteLine("tarifa B");
+            }
+             else if (smmlv > smmlv3 )
+            {
+                Console.WriteLine("tarifa C");
+            }
+
+        }
+    }
+}
---
+++ program32.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ingrse su salario");
+            double smmlv = double.Parse(Console.ReadLine());
+            double smmlv2 = 877803 * 2;
+            double smmlv3 = 877803 * 5;
+
+            if (smmlv < smmlv2)
+            {
+                Console.WriteLine("tarifa A con cuota moderada de 3.400");
+            }
+            else if (smmlv2 <= smmlv && smmlv <= smmlv3)
+            {
+                Console.WriteLine("tarifa B con cuota moderada de 13.500");
+            }
+            else if (smmlv > smmlv3)
+            {
+                Console.WriteLine("tarifa C con cuota moderada de 35.600");
+            }
+
+        }
+    }
+}
