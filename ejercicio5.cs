efe53620eaf35e84c7979675a7f5cdacf3de5e31
marianaCano <60165489+marianaCano@users.noreply.github.com>
2020-02-05 15:05:40 -0500

    Add files via upload

---
+++ ejercicio5.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace tarea4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese los datos correspondientes");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int blancos = int.Parse(Console.ReadLine());
+            int anulados = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+
+            int mayores = (n * p) / 100;
+            int total = (a + b + blancos + anulados);
+            int d = 0;
+            
+            if (a > b)
+            {
+                d = a - b;
+            }
+            else
+            {
+                 d = b - a;
+            }
+
+            double x = n * 0.30;
+            double y = n * 0.10;
+
+            if (((total > n ) || (d < y)) && (total < x))
+            {
+                Console.WriteLine("las elecciones deben ser ejecutadas nuevamente");
+            }
+            else
+            {
+                if (a > b)
+                {
+                    Console.WriteLine("a es el partido ganador");
+                }
+                if (b > a)
+                {
+                    Console.WriteLine("b es el partido ganador");
+                }
+            }
+        }
+    }
+}
